2baa2c2b6da994576a2d6543b310f62f85c4d111
Arindam Tanti <arindamtanti123@gmail.com>
2021-08-20 19:16:19 +0530

    First Commit Of All File
    1. 1.jpg = Background Image
    2. index.html = HTML page
    3. README.md = README markdown
    4. scripts.js = Java Script Code
    5. style.css = CSS stylesheet for the app

---
+++ README.md
@@ -0,0 +1,40 @@
+# Basic WebApp For Beginner Project-1
+
+``
+This app is basically a countdown for new year's eve. This is my first webapp project that I have created.
+``
+
+----------------------------------------------------------------
+
+> This project helps to understand how a webpage adn it's components can be changed/customized as per requirement.
+
+---------------
+
+## STEPS :
+ - Create a folder as per your choice
+ - Make the basic structure of HTML page and name is as index.html.
+ - Write the stylesheet to give styling to the html page and name the stylesheet as style.css
+ - Write the javascript to put logic/ functionality in the HTML page.
+
+--------------
+
+### Languages Used : 
+
+1. HTML
+2. CSS
+3. JAVASCRIPT
+
+--------------
+
+### How To Use : 
+
+```
+1. Download the whole repo from github
+
+2. Extract it in a folder.
+
+3. Run the index.html from the folder.
+```
+
+## References And Thanks To 
+[Project Link](https://github.com/Vanshikapandey30/Countdown-js)
---
+++ script.js
@@ -0,0 +1,34 @@
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours'); 
+const minE1 = document.getElementById('min');
+const secE1 = document.getElementById('sec');
+
+// Initializing the first date of new year
+const newyears = '1 Jan 2022';
+
+function countdown() {
+    const nyd = new Date(newyears); // initialize the first date time and sec
+    const currentdate = new Date();  // initialize the current date time and sec
+
+    const sec = (nyd - currentdate) / 1000; // the result will be in form of seconds
+
+    // Math operations on the variables to get the result of remaining time till new year
+    const days = Math.floor(sec / 3600 / 24); 
+    const hours = Math.floor(sec / 3600) % 24;
+    const min = Math.floor(sec / 60) % 60;
+    const seco = Math.floor(sec % 60);
+
+    //Printing the result in the webpage
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = format(hours);
+    minE1.innerHTML = format(min);
+    secE1.innerHTML = format(seco);
+}
+function format(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
+//initial call
+countdown();
+
+
+setInterval(countdown, 1000)
